61f3e1b4c78edb3fa0c8b12bb04fbbbebf1936fb
VMMO23 <victormanuelmarinoq@gmail.com>
2026-07-07 10:38:35 -0500

    Login (Se avanza en la estructura y diseño del Login)

--- Almacen_la_89/src/views/Login.jsx
+++ Almacen_la_89/src/views/Login.jsx
@@ -1,9 +1,22 @@
 import {Navbar} from "../components/Navbar"
+import "../styles/Login.css"
 
 function Login(){
 
     return(
-        <Navbar/>
+
+        <form className="login-container">
+
+            <h1 className="login-title"> Iniciar Sesión</h1>
+            
+            <label className="login-label" >Email:</label>
+            <input className="login-input"  type="email" required  id="email" placeholder="Correo"></input>
+            <br></br>
+            <label className="login-label" >Contraseña:</label>
+            <input className="login-input" type="password" id="contraseña" placeholder="Contraseña"></input>
+            <br></br>
+            <button className="login-button" type="submit"> Iniciar sesión</button>
+        </form>    
     )
 
 
